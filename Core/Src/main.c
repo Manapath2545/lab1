cd1c291d9be1fe1a6375d5ff4fd85fc293ec9295
Manapath2545 <122882545+Manapath2545@users.noreply.github.com>
2023-02-08 21:02:33 +0700

    name

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+// Manapath Klinpho
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
